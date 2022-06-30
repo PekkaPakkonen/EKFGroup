2856eca87968e5aafdc0ace77c4b1127282b49c9
PekkaPakkonen <redxray2010@gmail.com>
2022-06-30 23:47:38 +0300

    30.06 - update files for SeleniumGrid

--- EKFGroup/src/test/java/cartEKF1.java
+++ EKFGroup/src/test/java/cartEKF1.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -19,8 +18,8 @@ public class cartEKF1 {
     private WebDriver webDriver;
     private mainPage mainP;
 
-    @BeforeTest
-    public void prep() throws MalformedURLException, InterruptedException {
+    @Test
+    public void click() throws MalformedURLException, InterruptedException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setBrowserName("firefox");
         caps.setPlatform(Platform.UNIX);
@@ -31,10 +30,6 @@ public class cartEKF1 {
         webDriver.get("https://ekfgroup.com/cart");
         new WebDriverWait(webDriver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.elementToBeClickable(mainP.getDistributor()));
-    }
-
-    @Test
-    public void click() {
         String cb = "mcb6-1-10C-av";
         webDriver.findElement(By.cssSelector(".row .px-4 .form-control[placeholder=\"Артикул\"]"))
                 .sendKeys(cb);
@@ -42,11 +37,12 @@ public class cartEKF1 {
         new WebDriverWait(webDriver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".col-md-2.col-4 .img-fluid")));
         Assert.assertEquals(cb, webDriver.findElement(By.cssSelector(".col-md-2.col-4 .img-fluid")).getAttribute("alt"));
+        webDriver.quit();
     }
 
     @AfterTest
     public void stopBrowser() {
-        webDriver.quit();
+
     }
 
 }
